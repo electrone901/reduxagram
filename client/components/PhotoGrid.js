a7a0cd396846cbbb123907392a2e1dd037f88311
gaynor-landmark <g.l.landmark@gmail.com>
2016-06-30 22:39:40 +1200

    display photos and buttons

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,15 +1,13 @@
 import React from 'react'
+import Photo from './Photo'
 
 const PhotoGrid = React.createClass(
   {
     render() {
       return (
-      <div>
+      <div className='photo-grid'>
 
-        <h2>PhotoGrid</h2>
-        <pre>
-          {JSON.stringify(this.props.posts, null,' ')}
-        </pre>
+        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post}/>)}
       </div>
     )}
   }
