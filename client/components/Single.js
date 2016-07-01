8f4a2d3cfb3345bbb90454f2cd8e388b771c5ec7
gaynor-landmark <g.l.landmark@gmail.com>
2016-07-01 20:33:35 +1200

    add the increment reducer

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Photo from './Photo'
+//import Comments from './Comments'
 
 const Single = React.createClass(
   {
     render() {
       return (
-      <div>
+      <div className='single-photo'>
 
         <h2>Single</h2>
       </div>
